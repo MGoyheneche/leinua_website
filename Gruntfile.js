9056e875043411e3dcde6a34dbc63716cb9b638c
Mathias Goyheneche <mathias.goyheneche@gmail.com>
2017-12-11 19:30:54 +0100

    website

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,10 +3,20 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
 
   grunt.initConfig({
+    express: {
+      all: {
+        options: {
+          bases: ['./app/'],
+          port: 8080,
+          hostname: '0.0.0.0',
+          livereload: true,
+          open: true
+        }
+      }
+    },
     sass: {
       options: {
-        sourceMap: true
-        },
+        sourceMap: true },
       dist: {
         files: {
           'app/styles/css/main.css': 'app/styles/scss/main.scss'
@@ -19,9 +29,15 @@ module.exports = function(grunt) {
         tasks: ['sass'],
         options: {},
       },
+      all: {
+        files: '**/*.html',
+        options: {
+          livereload: true
+        }
+      }
     },
   });
 
-  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('default', ['express', 'watch']);
 
 };
